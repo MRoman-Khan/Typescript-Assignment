e797ea44bded23e48fee137683c185a657235a46
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 15:52:43 +0500

    Create adding_comments.js

---
+++ Q10/adding_comments.js
@@ -0,0 +1,8 @@
+// Program written by [Muhammad Roman]
+// Date: [05-09-2023]
+// Store your favorite number in a variable.
+var favoriteNumber = 9; // Replace with your favorite number
+// Create a message revealing your favorite number.
+var message = "My favorite number is ".concat(favoriteNumber, ".");
+// Print the message to the console.
+console.log(message);
