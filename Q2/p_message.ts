6962ea1cc19ef1430ab894ed6818b451c52309f5
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 11:29:29 +0500

    Create p_message.ts
    update

---
+++ Q2/p_message.ts
@@ -0,0 +1,8 @@
+// Store the person's name in a variable
+const personName = "Roman";
+
+// Create a message
+const message = `Hello ${personName}, would you like to learn some Python today?`;
+
+// Print the message
+console.log(message);
