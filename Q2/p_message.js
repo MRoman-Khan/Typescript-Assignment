7bf3a89713c769c551b3cac66139aaa3b22169a2
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 12:03:05 +0500

    Create p_message.js

---
+++ Q2/p_message.js
@@ -0,0 +1,6 @@
+// Store the person's name in a variable
+var personName = "Roman";
+// Create a message
+var message = "Hello ".concat(personName, ", would you like to learn some Python today?");
+// Print the message
+console.log(message);
