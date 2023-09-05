75c7475ae0ef6bb5b420bdf5410ecedef00cf3b1
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 12:18:43 +0500

    Create famous_qoute.ts

---
+++ Q4/famous_qoute.ts
@@ -0,0 +1,7 @@
+// Define the quote and its author
+const quote = "Imagination is more important than knowledge.";
+const author = "Albert Einstein";
+
+// Print the quote and author
+console.log(`"${quote}"`);
+console.log(`- ${author}`);
