0e06f07326c9463d3765d5fe0f530190af7d2982
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 13:28:11 +0500

    Create famous_qoute2.js

---
+++ Q5/famous_qoute2.js
@@ -0,0 +1,8 @@
+// Store the famous person's name in a variable
+var famous_person = "Albert Einstein";
+// Define the quote by the famous person
+var quote = "Imagination is more important than knowledge.";
+// Compose the message including the quote and author
+var message = "\"".concat(quote, "\"\n- ").concat(famous_person);
+// Print the message
+console.log(message);
