9f3574551e0de17d096fa238d0f9f394b8a46495
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 13:27:14 +0500

    Create famous_qoute2.ts

---
+++ Q5/famous_qoute2.ts
@@ -0,0 +1,11 @@
+// Store the famous person's name in a variable
+const famous_person = "Albert Einstein";
+
+// Define the quote by the famous person
+const quote = "Imagination is more important than knowledge.";
+
+// Compose the message including the quote and author
+const message = `"${quote}"\n- ${famous_person}`;
+
+// Print the message
+console.log(message);
