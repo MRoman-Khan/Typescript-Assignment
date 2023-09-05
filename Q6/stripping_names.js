e984a900c314b8112ae0c3a8a9ee737178064bdc
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 13:29:16 +0500

    Create stripping_names.js

---
+++ Q6/stripping_names.js
@@ -0,0 +1,8 @@
+var personNameWithWhitespace = "\tAli \nKhan\t"; // Sample name with whitespace
+// Print the name with whitespace
+console.log("Name with whitespace:");
+console.log(personNameWithWhitespace);
+// Strip whitespace and print the name without it
+var strippedName = personNameWithWhitespace.trim();
+console.log("\nName after stripping whitespace:");
+console.log(strippedName);
