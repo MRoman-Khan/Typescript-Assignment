d4dd73534dd5e7ef57699fb0db899056ee9fb407
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 12:19:30 +0500

    Create famous_qoute.js

---
+++ Q4/famous_qoute.js
@@ -0,0 +1,6 @@
+// Define the quote and its author
+var quote = "Imagination is more important than knowledge.";
+var author = "Albert Einstein";
+// Print the quote and author
+console.log("\"".concat(quote, "\""));
+console.log("- ".concat(author));
