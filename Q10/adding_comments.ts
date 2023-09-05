23d3c1acada0a3e5953f8d50bc669080544cf94f
Muhammad Roman <roman.online67@gmail.com>
2023-09-05 15:52:36 +0500

    Create adding_comments.ts

---
+++ Q10/adding_comments.ts
@@ -0,0 +1,11 @@
+// Program written by [Muhammad Roman]
+// Date: [05-09-2023]
+
+// Store your favorite number in a variable.
+const favoriteNumber = 9; // Replace with your favorite number
+
+// Create a message revealing your favorite number.
+const message = `My favorite number is ${favoriteNumber}.`;
+
+// Print the message to the console.
+console.log(message);
