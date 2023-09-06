9a032f7f47d4103c9dec1d6b00574fb2d5ef1ba8
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:21:02 +0500

    Create greetings.js

---
+++ Q12/greetings.js
@@ -0,0 +1,7 @@
+// Define an array called 'names' to store your friends' names
+var names = ["Ali", "Waqas", "Zuhaib", "Roman"];
+// Iterate through the 'names' array and print a personalized message to each person
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log("Hello, ".concat(name_1, "! Have a great day."));
+}
