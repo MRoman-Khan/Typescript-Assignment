51f089fdf0b81daf6945acea85ed7cbf9e4de45f
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:25:24 +0500

    Create own_array.js

---
+++ Q13/own_array.js
@@ -0,0 +1,13 @@
+// Define an array called 'cars' to store examples of car brands and models
+var cars = [
+    "Honda Civic",
+    "Toyota Camry",
+    "Ford Mustang",
+    "Tesla Model 3",
+    "BMW M3"
+];
+// Iterate through the 'cars' array and print statements about each car
+for (var _i = 0, cars_1 = cars; _i < cars_1.length; _i++) {
+    var car = cars_1[_i];
+    console.log("I would like to own a ".concat(car, "."));
+}
