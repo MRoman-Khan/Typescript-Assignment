d827fd0f5955d9c4e1e65c11a453388d4769d27f
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:14:41 +0500

    Create names.js

---
+++ Q11/names.js
@@ -0,0 +1,7 @@
+// Define an array called 'names' to store your friends' names
+var names = ["Ali", "waqas", "zuhaib", "Roman"];
+// Iterate through the 'names' array and print each person's name
+for (var _i = 0, names_1 = names; _i < names_1.length; _i++) {
+    var name_1 = names_1[_i];
+    console.log(name_1);
+}
