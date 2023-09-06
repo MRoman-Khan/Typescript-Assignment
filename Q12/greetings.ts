e9255caf6c0cf275c5f2cd60a5fc457c22100d7a
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:20:59 +0500

    Create greetings.ts

---
+++ Q12/greetings.ts
@@ -0,0 +1,7 @@
+// Define an array called 'names' to store your friends' names
+const names: string[] = ["Ali", "Waqas", "Zuhaib", "Roman"];
+
+// Iterate through the 'names' array and print a personalized message to each person
+for (const name of names) {
+  console.log(`Hello, ${name}! Have a great day.`);
+}
