079ac3008724ffb025727347c707762806a3c599
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:25:20 +0500

    Create own_array.ts

---
+++ Q13/own_array.ts
@@ -0,0 +1,14 @@
+// Define an array called 'cars' to store examples of car brands and models
+const cars = [
+    "Honda Civic",
+    "Toyota Camry",
+    "Ford Mustang",
+    "Tesla Model 3",
+    "BMW M3"
+  ];
+  
+  // Iterate through the 'cars' array and print statements about each car
+  for (const car of cars) {
+    console.log(`I would like to own a ${car}.`);
+  }
+  
