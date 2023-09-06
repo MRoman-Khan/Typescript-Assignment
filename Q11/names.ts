2ffb71a4300fac3025a547fcebe5dcda666b2b01
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:14:36 +0500

    Create names.ts

---
+++ Q11/names.ts
@@ -0,0 +1,7 @@
+// Define an array called 'names' to store your friends' names
+const names: string[] = ["Ali", "waqas", "zuhaib", "Roman"];
+
+// Iterate through the 'names' array and print each person's name
+for (const name of names) {
+  console.log(name);
+}
