fb8e23d4b98f0660bbcdcdb65282f4873aa82507
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:28:56 +0500

    Create guest_list.js

---
+++ Q14/guest_list.js
@@ -0,0 +1,6 @@
+var guests = ["Ali", "Waqas", "zuhaib"];
+// Iterate through the 'guests' array and print messages inviting each person to dinner
+for (var _i = 0, guests_1 = guests; _i < guests_1.length; _i++) {
+    var guest = guests_1[_i];
+    console.log("Dear ".concat(guest, ", I would like to invite you to dinner. It would be an honor to have you as my guest."));
+}
