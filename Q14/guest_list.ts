e222349f81b79f1b11038531e6773065d4d6fa20
Muhammad Roman <roman.online67@gmail.com>
2023-09-06 11:28:54 +0500

    Create guest_list.ts

---
+++ Q14/guest_list.ts
@@ -0,0 +1,7 @@
+
+const guests = ["Ali", "Waqas", "zuhaib"];
+
+// Iterate through the 'guests' array and print messages inviting each person to dinner
+for (const guest of guests) {
+  console.log(`Dear ${guest}, I would like to invite you to dinner. It would be an honor to have you as my guest.`);
+}
