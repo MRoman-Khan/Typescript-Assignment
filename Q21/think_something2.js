2b87cef37cc9155b3954f364b4a32990894b32d7
Muhammad Roman <roman.online67@gmail.com>
2023-09-13 13:32:22 +0500

    Create think_something2.js

---
+++ Q21/think_something2.js
@@ -0,0 +1,33 @@
+// Create objects to store information about countries
+var usa = {
+    name: "United States",
+    capital: "Washington, D.C.",
+    population: 331002651,
+    currency: "United States Dollar (USD)",
+};
+var canada = {
+    name: "Canada",
+    capital: "Ottawa",
+    population: 37742154,
+    currency: "Canadian Dollar (CAD)",
+};
+var uk = {
+    name: "United Kingdom",
+    capital: "London",
+    population: 67886011,
+    currency: "Pound Sterling (GBP)",
+};
+// Print information about the countries
+console.log("Country Information:");
+console.log("1. Name:", usa.name);
+console.log("   Capital:", usa.capital);
+console.log("   Population:", usa.population);
+console.log("   Currency:", usa.currency);
+console.log("2. Name:", canada.name);
+console.log("   Capital:", canada.capital);
+console.log("   Population:", canada.population);
+console.log("   Currency:", canada.currency);
+console.log("3. Name:", uk.name);
+console.log("   Capital:", uk.capital);
+console.log("   Population:", uk.population);
+console.log("   Currency:", uk.currency);
